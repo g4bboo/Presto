4e3615b98514712a91611fbaf7f595fef0de46e2
Gabriele  Tomba <gabriele.tomba@gmail.com>
2021-07-13 14:43:41 +0200

    modifiche nuove

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('scroll', function() {
-    const navbar = document.querySelector('.p-navbar');
-    if(window.scrollY > 250){
-        navbar.classList.add('active');
-    } else {
-        navbar.classList.remove('active');
-    }
-})
